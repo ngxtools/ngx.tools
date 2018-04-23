902266e975b1826df8007152bc1943c271f52876
Wassim CHEGHAM <github@wassimchegham.com>
2018-04-23 23:24:32 +0200

    chore: add tooltip mat module

--- src/app/core/mat/mat.module.ts
+++ src/app/core/mat/mat.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatCardModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatTooltipModule
+} from '@angular/material';
 
-const MAT_MODULES = [MatInputModule, MatToolbarModule, MatCardModule, MatButtonModule, MatFormFieldModule];
+const MAT_MODULES = [MatTooltipModule, MatIconModule, MatInputModule, MatToolbarModule, MatCardModule, MatButtonModule, MatFormFieldModule];
 
 @NgModule({
   imports: MAT_MODULES,
