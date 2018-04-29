1252c0b63e6c888c7651eb8c7a45adf2f32ad4fd
Wassim CHEGHAM <github@wassimchegham.com>
2018-04-30 00:55:35 +0200

    chore: setup test with chromeheadless

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,18 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['customLaunchers'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
     singleRun: false
   });
 };
