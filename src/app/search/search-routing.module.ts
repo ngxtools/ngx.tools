5cf65bd2a6ace7c58c445b7177c454eff87b54b6
Wassim Chegham <github@wassim.app>
2018-11-27 17:01:22 +0100

    fix: lazy loading bug (#22)
    * chore: wip
    
    * build: update package-lock.json

--- src/app/search/search-routing.module.ts
+++ src/app/search/search-routing.module.ts
@@ -1,6 +1,5 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search/search.component';
 
 export const routes: Routes = [{ path: '', component: SearchComponent }];
