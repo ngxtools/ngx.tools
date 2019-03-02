9387f0782eedadd48843bf87c71e05a8198ef231
Wassim Chegham <github@wassim.app>
2019-03-02 01:55:08 +0100

    refactor: fix the behavior of the infinite scroll

--- src/app/search/infinite-scroll.directive.ts
+++ src/app/search/infinite-scroll.directive.ts
@@ -1,24 +1,35 @@
-import { AfterViewInit, Directive, EventEmitter, Inject, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Directive,
+  EventEmitter,
+  Inject,
+  Output
+} from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
 import { fromEvent } from 'rxjs/internal/observable/fromEvent';
 import { debounceTime, filter, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Directive({
-  selector: '[app-infinite-scroll]',
+  selector: '[appInfiniteScroll]'
 })
 export class InfiniteScrollDirective implements AfterViewInit {
   @Output() scroll = new EventEmitter();
 
-  private scrollEvent$;
-  private userScrolledDown$;
-  threshold = 300;
+  private scrollEvent$: Observable<Event>;
+  private userScrolledDown$: Observable<number>;
+  threshold = 500;
 
-  constructor(@Inject(DOCUMENT) private document) {
+  constructor(@Inject(DOCUMENT) private document: any) {
     this.scrollEvent$ = fromEvent(window, 'scroll');
     this.userScrolledDown$ = this.scrollEvent$.pipe(
       map(() => window.scrollY),
       debounceTime(200),
-      filter((current: number) => current >= document.body.clientHeight - window.innerHeight - this.threshold),
+      filter(
+        (current: number) =>
+          current >=
+          this.document.body.clientHeight - window.innerHeight - this.threshold
+      )
     );
   }
 
