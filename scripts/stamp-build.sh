51685936d944deecaff253349dafce7e03f80e7e
Wassim Chegham <1699357+manekinekko@users.noreply.github.com>
2020-05-15 18:29:07 +0200

    ci: update stamp script

--- scripts/stamp-build.sh
+++ scripts/stamp-build.sh
@@ -1,16 +1,24 @@
+#!/bin/bash
 
-#!/bin/sh
+set -o errexit
+set -o pipefail
 
-b=`git rev-parse --abbrev-ref HEAD`
-v=`git rev-parse --short HEAD`
-version="$b+sha.$v"
+if [ -z "$GITHUB_SHA" ]
+then
+   b=`git rev-parse --abbrev-ref HEAD`
+   v=`git rev-parse --short HEAD`
+   version="$b+sha.$v"
+else
+   v=`echo $GITHUB_SHA | cut -c1-8`
+   version="build: $v"
+fi
 
 ## replease _BUILD_HASH_ with the current build number
-perl -i -pe "s/_BUILD_HASH_/$version/g" dist/*/main.*.js
+perl -i -pe "s/_BUILD_HASH_/$version/g" dist/ngx.tools/*.js
 
 status=$?
 if [ $status -eq 0 ];then
-   echo "Build was stamped: $version"
+   echo ">> Build was stamped: $version"
 else
-   echo "Could not stamp this build!"
+   echo ">> Could not stamp this build!"
 fi
