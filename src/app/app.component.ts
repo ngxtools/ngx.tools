83ac855432f68298f45947cde00f8f2af8279d3d
Wassim Chegham <github@wassim.dev>
2023-03-07 01:51:51 +0100

    feat: add dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Event, Router, RoutesRecognized } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { PromptUpdateService } from './prompt-update.service';
+import { ThemeManagerService } from './shared/theme-manager.service';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +11,30 @@ import { PromptUpdateService } from './prompt-update.service';
 })
 export class AppComponent {
   version = environment.version;
+  isDarkMode = signal(false);
+  shouldShowBackButton = signal(false);
 
-  constructor(private promptUpdateService: PromptUpdateService) {
+  constructor(
+    private promptUpdateService: PromptUpdateService,
+    private themeManager: ThemeManagerService, router: Router) {
     this.promptUpdateService.check();
+    this.isDarkMode.set(this.themeManager.isDarkMode);
+
+    router.events.subscribe((event: Event) => {
+      if (event instanceof RoutesRecognized) {
+        const { url } = event
+        this.shouldShowBackButton.set(url.startsWith('/pkg'));
+      }
+    });
   }
+
+  toggleDarkTheme() {
+    this.themeManager.toggleDarkTheme();
+    this.isDarkMode.mutate(() => this.isDarkMode.set(this.themeManager.isDarkMode));
+  }
+
+  navigateBack() {
+    window.history.back();
+  }
+
 }
---
+++ src/app/shared/theme-manager.service.ts
@@ -0,0 +1,74 @@
+
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class ThemeManagerService {
+  isDarkMode = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.isDarkMode = this.#readThemePreference('ngxtools-theme-mode') === 'dark';
+    if (this.isDarkMode) {
+      this.#enableDarkTheme();
+    }
+  }
+
+  toggleDarkTheme() {
+    if (this.isDarkMode) {
+      this.#disableDarkTheme();
+    } else {
+      this.#enableDarkTheme();
+    }
+
+    this.isDarkMode = !this.isDarkMode;
+    this.#saveThemePreference('ngxtools-theme-mode', this.isDarkMode);
+  }
+
+  #disableDarkTheme() {
+    this.#removeStyle('dark-theme');
+    this.document.body.classList.remove('dark-theme');
+  }
+
+  #enableDarkTheme() {
+    const href = 'dark-theme.css';
+    this.#getLinkElementForKey('dark-theme').setAttribute('href', href);
+    this.document.body.classList.add('dark-theme');
+  }
+
+  #removeStyle(key: string) {
+    const existingLinkElement = this.#getExistingLinkElementByKey(key);
+    if (existingLinkElement) {
+      this.document.head.removeChild(existingLinkElement);
+    }
+  }
+
+  #saveThemePreference(key: string, status: boolean) {
+    localStorage.setItem(key, status ? 'dark' : 'light');
+  }
+
+  #readThemePreference(key: string) {
+    return localStorage.getItem(key) || 'light';
+  }
+
+  #getLinkElementForKey(key: string) {
+    return this.#getExistingLinkElementByKey(key) || this.#createLinkElementWithKey(key);
+  }
+
+  #getExistingLinkElementByKey(key: string) {
+    return this.document.head.querySelector(
+      `link[rel="stylesheet"].${this.#getClassNameForKey(key)}`
+    );
+  }
+
+  #createLinkElementWithKey(key: string) {
+    const linkEl = this.document.createElement('link');
+    linkEl.setAttribute('rel', 'stylesheet');
+    linkEl.classList.add(this.#getClassNameForKey(key));
+    this.document.head.appendChild(linkEl);
+    return linkEl;
+  }
+
+  #getClassNameForKey(key: string) {
+    return `theme-${key}`;
+  }
+}
